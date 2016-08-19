c4825518d3374fa5ec16b09ddfcc3e17075dea6d
gaogaotiantian <gaogaotiantian@hotmail.com>
2016-08-19 16:29:21 -0700

    Add a potential remove list and gitignore file

--- GetUser.py
+++ GetUser.py
@@ -11,23 +11,24 @@ def GetUsers(content):
 
 content = open('test.html', 'r').read()
 users = GetUsers(content)
-files = os.listdir('./')
+filelist = open("filelist.txt", 'r').read().splitlines()
+storeFile = open("PotentialRemove.txt", "w")
 
 name_pat = re.compile(r"(gg|GG).")
 badUsers = []
+#users = [u"GG-荷兰/津-24-斜"]
 for user in users:
-    possible_name = re.split("[-~_ ]+", user)
-    for n in possible_name:
-        has = 0
-        for f in files:
-            if n.strip().encode('utf-8') in f:
-                has = 1
-                break
-        if has == 0:
-            badUsers.append(user)
+    possible_name = re.split("[-~_ ]+", user)[-1]
+    for f in filelist:
+        if possible_name.strip().encode('utf-8') in f:
+            has = 1
             break
+    else:
+        badUsers.append(user)
 
-print len(badUsers)
+storeFile.write(str(len(badUsers))+'\n')
 
 for user in badUsers:
-    print user
+    storeFile.write(user.encode('utf-8')+'\n')
+
+
